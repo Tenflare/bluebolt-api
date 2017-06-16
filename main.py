3c3c44f3fab153fdfd7e556d705a812d99daa324
kecorbin <kecorbin@cisco.com>
2017-06-16 01:04:42 -0500

    quick refactor + new switch resource

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
 import telnetlib
-import time
-import requests
-import time
 from flask import Flask
 from flask_restful import Resource, Api
-import argparse
-import sys
 
 
 app = Flask(__name__)
@@ -17,47 +12,74 @@ _NEWLINE = '\r\n'
 
 BLUEBOLT_IP = os.getenv("HOST")
 TELNET_PORT = 23
+OUTLETS = 8
 
 
 def login():
-    connection = False
     session = telnetlib.Telnet(BLUEBOLT_IP, TELNET_PORT)
     print "Successfully Logged in to Panamax"
     return session
 
 
-def send_command(session, outlet, command):
-    session.write('#CYCLE {}:{}\r\n'.format(outlet, command))
-    return "OK"
-
-
-class Command(Resource):
+class Switch(Resource):
     """
-    Generic API resource for interacting with Bluebolt devices.
+    # SWITCH 1 ON/OFF
 
-    Power cycle outlet 1
-
-    #CYCLE 1:5
-    /api/cycle/1/5
-
-    Power cycle outlet 2
-
-    #CYCLE 2:5
-    /api/cycle/2/5
-
-
+    :param session:
+    :param outlet:
+    :param state:
+    :return:
     """
-    # TODO Need to determine if we can get status
-
-
-    def post(self, outlet, command):
+    def post(self, outlet, state):
         session = login()
-        send_command(session, outlet, command)
-        # Need better validation here, for now we assume the command worked
-        return "OK"
+
+        session.write('!SWITCH {} {}\r\n'.format(outlet, str(state).upper()))
+
+        bb = session.expect(["OUTLET[1-9] = O[NF]."], 3)
+        try:
+            outlet, status = bb[2].strip().split("=")
+            print outlet, status
+            return {int(outlet[7]): status.strip()}
+
+        except:
+
+            return {"error": "could not parse bluebolt response"}
+
+
+class OutletStatus(Resource):
+
+    def get(self):
+        session = login()
+        session.write('?OUTLETSTAT\r\n')
+
+        bb = session.expect(['OUTLET{} = O[NF].'.format(OUTLETS)], 5)
+        resp = dict()
+        try:
+            outlets = bb[2].split('\r\n')
+            for outlet in outlets:
+                outlet, status = outlet.strip().split('=')
+                resp[int(outlet[7])] = status.strip()
+            return resp
+        except:
+            return {"error": "could not parse bluebolt response"}
+
+
+class Cycle(Resource):
+    def post(self, outlet, delay):
+        session = login()
+        try:
+            session.write('#CYCLE {}:{}\r\n'.format(outlet, delay))
+            # Need better validation here, for now we assume the command worked
+            # as long delays (15+ seconds) would be a valid use case, and we don't
+            # want to wait around
+            return {outlet: "rebooting"}
+        except:
+            return {"error": "something went wrong"}
 
 # Expose Bluebolt commands directly as API
-api.add_resource(Command, '/api/<string:outlet>/<string:command>')
+api.add_resource(Cycle, '/cycle/<string:outlet>/<string:delay>')
+api.add_resource(Switch, '/switch/<string:outlet>/<string:state>')
+api.add_resource(OutletStatus, '/outlet/status')
 
 if __name__ == '__main__':
 
